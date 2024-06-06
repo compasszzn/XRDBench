e3242a50cbf8946096bab4b7a5377833499fee93
compasszzn <1065007960@qq.com>
2024-06-06 11:40:26 +0000

    update

--- scripts/IUCrj_CNN.sh
+++
@@ -1,8 +0,0 @@
-task=crysystem
-model=IUCrj_CNN
-epochs=100
-lr=0.1
-batch_size=64
-gpu=7
-
-python -u main.py --model $model --task $task --epochs $epochs --lr $lr --batch_size $batch_size --gpu $gpu
--- scripts/NPCNN.sh
+++
@@ -1,8 +0,0 @@
-task=crysystem
-model=NPCNN
-epochs=100
-lr=0.00025
-batch_size=128
-gpu=7
-
-python -u main.py --model $model --task $task --epochs $epochs --lr $lr --batch_size $batch_size --gpu $gpu
